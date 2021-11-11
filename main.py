aa0d2bdffdf887160ddded38bf0c02a3903591d0
Sandeep Chauhan <sandeepc701@gmail.com>
2021-11-11 11:59:20 +0530

    "Added Selection Sort Code  !!!!"

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hi this is a test python project !!!")
+print("{}Hi this is a test python project!!!{}".format("\n\n\n\n","\n\n\n\n"))
+
+def SelectionSort(l):
+    for i in range(len(l)):
+        temp=l[i];
+        for j in range(i,len(l)):
+            if l[j]<temp:
+                l[i]=l[j]
+                l[j]=temp;
+
+
+l=[2,4,1,7,6,10,0]
+
+SelectionSort(l);
+
+print(l);
+
+
